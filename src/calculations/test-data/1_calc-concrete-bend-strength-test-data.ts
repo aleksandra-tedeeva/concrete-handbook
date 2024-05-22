aa973bf043bdc3ffb1acd557ddaa22e49f0c859e
aleksandra-tedeeva <96416646+aleksandra-tedeeva@users.noreply.github.com>
2024-05-22 12:56:23 +0700

    error 0.1 actually works too

--- src/calculations/test-data/1_calc-concrete-bend-strength-test-data.ts
+++ src/calculations/test-data/1_calc-concrete-bend-strength-test-data.ts
@@ -11,7 +11,7 @@ export interface CalculateConcreteBendStrengthTestData {
 }
 
 // допустимая погрешность в %
-export const control_acceptable_error = 1;
+export const control_acceptable_error = 0.1;
 
 export const testData: CalculateConcreteBendStrengthTestData[] = [
   // 1
